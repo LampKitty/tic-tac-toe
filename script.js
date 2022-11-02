1650f866917433083778360c83495fa00e0c37de
Ranpukatu <ranpukatu@gmail.com>
2022-11-02 07:50:30 +0200

    tictactoe board css ready

---
+++ script.js
@@ -0,0 +1,21 @@
+const cell = document.querySelectorAll('.gameboard>div');
+
+const gameBoard = (() => {
+let playField = 
+['','','',
+'','','',
+'','','',]
+return {playField,};
+})();
+
+const displayController = (() => {
+
+})();
+
+const player = (name) => {
+    const placeMark = () => {};
+    return {
+        placeMark,
+    }
+}
+
