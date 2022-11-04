c3add559926c28c9283b1b69b799b0d9403cdc44
Ranpukatu <ranpukatu@gmail.com>
2022-11-04 18:07:02 +0200

    add circle sign

--- script.js
+++ script.js
@@ -4,38 +4,54 @@ const cells = document.querySelectorAll('.gameboard>div');
 
 const gameBoard = (() => {
 
-let playField = 
-['','','',
-'','','',
-'','','',]
+    let playField =
+        ['', '', '',
+        '', '', '',
+        '', '', '',];
+
+    const populateArr = (cells, cell, sign) =>
+        playField[Array.from(cells).indexOf(cell, 0)] = sign;
 
 
-
-return {playField,};
+    return { playField, populateArr, isWinner };
 })();
 
 const displayController = (() => {
 
-    cells.forEach(cell => cell.addEventListener('click', function() {
-   
-        if(cell.lastChild != null) {
+    let lastSign = '';
+
+    cells.forEach(cell => cell.addEventListener('click', function () {
+
+        if (cell.lastChild != null) {
             return;
         }
 
-        let cross = document.createElement('img');
-        cross.setAttribute('style','width: 100px; height: 100px;')
-        cross.src = 'img/cross.png';
-        cell.appendChild(cross);
-
-        gameBoard.playField[Array.from(cells).indexOf(cell, 0)] = 'x';
+        if(lastSign === 'x'){
+            let circle = document.createElement('img');
+            circle.src = 'img/circle.png';
+            circle.setAttribute('style', 'width: 100px; height: 100px;');
+            gameBoard.populateArr(cells, cell, '0');
+            cell.appendChild(circle);
+            lastSign = '0';
+        }
+        else if(lastSign === '0' || lastSign === ''){
+            let cross = document.createElement('img');
+            cross.src = 'img/cross.png';
+            cross.setAttribute('style', 'width: 100px; height: 100px;');
+            gameBoard.populateArr(cells, cell, 'x');
+            cell.appendChild(cross);
+            lastSign = 'x';
+        }
         
-    }));    
+
+        console.log(gameBoard.playField);
+    }));
 
     return {}
 })();
 
 const player = (name) => {
-    const placeMark = () => {};
+    const placeMark = () => { };
     return {
         placeMark,
     }
