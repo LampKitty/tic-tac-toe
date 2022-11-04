0921768e3149667571d42dbafdc770cec4034ad2
Ranpukatu <ranpukatu@gmail.com>
2022-11-04 07:25:56 +0200

    populate array on click

--- script.js
+++ script.js
@@ -3,15 +3,35 @@ const cells = document.querySelectorAll('.gameboard>div');
 
 
 const gameBoard = (() => {
+
 let playField = 
 ['','','',
 '','','',
 '','','',]
+
+
+
 return {playField,};
 })();
 
 const displayController = (() => {
 
+    cells.forEach(cell => cell.addEventListener('click', function() {
+   
+        if(cell.lastChild != null) {
+            return;
+        }
+
+        let cross = document.createElement('img');
+        cross.setAttribute('style','width: 100px; height: 100px;')
+        cross.src = 'img/cross.png';
+        cell.appendChild(cross);
+
+        gameBoard.playField[Array.from(cells).indexOf(cell, 0)] = 'x';
+        
+    }));    
+
+    return {}
 })();
 
 const player = (name) => {
@@ -21,15 +41,3 @@ const player = (name) => {
     }
 }
 
-cells.forEach(cell => cell.addEventListener('click', function() {
-   
-    if(cell.lastChild != null) {
-        return;
-    }
-
-    let cross = document.createElement('img');
-    cross.setAttribute('style','width: 100px; height: 100px;')
-    cross.src = 'img/cross.png';
-    cell.appendChild(cross);
-    
-}));
