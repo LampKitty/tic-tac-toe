0572d96ab8be4db9d7965db1250df2ec63d0379a
Ranpukatu <ranpukatu@gmail.com>
2022-11-07 21:53:30 +0200

    rename input field disappears on click outside

--- script.js
+++ script.js
@@ -5,27 +5,27 @@ const gameBoard = (() => {
 
     let playField =
         ['', '', '',
-        '', '', '',
-        '', '', '',];
+            '', '', '',
+            '', '', '',];
 
     const populateArr = (cells, cell, sign) =>
         playField[Array.from(cells).indexOf(cell, 0)] = sign;
 
     const isWinner = (sign) => {
-        if((playField[0] === sign & playField[1] === sign & playField[2] === sign)
-        ||(playField[3] === sign & playField[4] === sign & playField[5] === sign)
-        ||(playField[6] === sign & playField[7] === sign & playField[8] === sign)) {
+        if ((playField[0] === sign & playField[1] === sign & playField[2] === sign)
+            || (playField[3] === sign & playField[4] === sign & playField[5] === sign)
+            || (playField[6] === sign & playField[7] === sign & playField[8] === sign)) {
             console.log('winner');
         }
-        else if 
-        ((playField[0] === sign & playField[3] === sign & playField[6] === sign)
-        ||(playField[1] === sign & playField[4] === sign & playField[7] === sign)
-        ||(playField[2] === sign & playField[5] === sign & playField[8] === sign)) {
+        else if
+            ((playField[0] === sign & playField[3] === sign & playField[6] === sign)
+            || (playField[1] === sign & playField[4] === sign & playField[7] === sign)
+            || (playField[2] === sign & playField[5] === sign & playField[8] === sign)) {
             console.log('winner');
         }
-        else if 
-        ((playField[0] === sign & playField[4] === sign & playField[8] === sign)
-        ||(playField[2] === sign & playField[4] === sign & playField[6] === sign)) {
+        else if
+            ((playField[0] === sign & playField[4] === sign & playField[8] === sign)
+            || (playField[2] === sign & playField[4] === sign & playField[6] === sign)) {
             console.log('winner');
         }
     }
@@ -43,7 +43,7 @@ const displayController = (() => {
             return;
         }
 
-        if(sign === '0'){
+        if (sign === '0') {
             let circle = document.createElement('img');
             circle.src = 'img/circle.png';
             circle.setAttribute('style', 'width: 100px; height: 100px;');
@@ -52,7 +52,7 @@ const displayController = (() => {
             gameBoard.isWinner(sign);
             sign = 'x';
         }
-        else if(sign === 'x'){
+        else if (sign === 'x') {
             let cross = document.createElement('img');
             cross.src = 'img/cross.png';
             cross.setAttribute('style', 'width: 100px; height: 100px;');
@@ -61,20 +61,32 @@ const displayController = (() => {
             gameBoard.isWinner(sign);
             sign = '0';
         }
-        
+
     }));
 
-    renameButtons.forEach(renameButton => 
-        renameButton.addEventListener('click', function() {
-            renameButton.previousElementSibling.classList.add('hidden'); 
+    renameButtons.forEach(renameButton =>
+        renameButton.addEventListener('click', function () {
+            renameButton.previousElementSibling.classList.add('hidden');
             renameButton.parentElement.firstElementChild.classList.add('visible');
         }
-    ))
+        ));
+
+    document.addEventListener('click', function(event) {
+        const renameInputs = document.querySelectorAll('.rename');
+        renameInputs.forEach(renameInput => {
+            if(!renameInput.contains(event.target)) {
+                renameInput.classList.remove('visible');
+                renameInput.nextElementSibling.innerHTML = renameInput.value;
+                renameInput.nextElementSibling.classList.remove('hidden');
+            }
+        })
+    }, true);
+
 
     return {}
 })();
 
 const Player = (name) => {
-    return{name};
+    return { name };
 }
 
